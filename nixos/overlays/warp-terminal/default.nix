313923a4a673b39e975a5952a0d3acce464ed719
Joseph Hanson <joe@veri.dev>
2024-07-01 14:10:52 -0500

    not needed

--- nixos/overlays/warp-terminal/default.nix
+++ nixos/overlays/warp-terminal/default.nix
@@ -9,8 +9,5 @@ in
       url = "https://releases.warp.dev/stable/v${versions.linux.version}/warp-terminal-v${versions.linux.version}-1-x86_64.pkg.tar.zst";
       hash = versions.linux.hash;
     };
-    # postInstall = ''
-    #   install -Dm644 $out/share/icons/hicolor/128x128/apps/dev.warp.Warp.png $out/share/icons/hicolor/128x128/apps/org.wezfurlong.wezterm.png
-    # '';
   });
 })
