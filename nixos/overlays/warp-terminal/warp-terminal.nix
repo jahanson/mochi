4a81e56111fc767fd8e5b3a098d10a175aafaa36
Joseph Hanson <joe@veri.dev>
2024-07-01 08:47:45 -0500

    use nixpkgs fetchurl

--- nixos/overlays/warp-terminal/warp-terminal.nix
+++ nixos/overlays/warp-terminal/warp-terminal.nix
@@ -5,9 +5,9 @@ in
 (final: prev: {
   warp-terminal = prev.warp-terminal.overrideAttrs (oldAttrs: { 
     version = versions.linux.version;
-    src = builtins.fetchurl {
+    src = prev.fetchurl {
       url = "https://releases.warp.dev/stable/v${versions.linux.version}/warp-terminal-v${versions.linux.version}-1-x86_64.pkg.tar.zst";
-      sha256 = versions.linux.hash;
+      hash = versions.linux.hash;
     };
   });
 })
