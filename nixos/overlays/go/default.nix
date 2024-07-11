ef0b4a46e118c75b43dee8ab543923503a93a829
Joseph Hanson <joe@veri.dev>
2024-07-11 08:47:29 -0500

    Update go and nixpkgs.

--- nixos/overlays/go/default.nix
+++ nixos/overlays/go/default.nix
@@ -1,13 +1,13 @@
 { ... }:
 let
-  finalVersion = "1.22.4";
+  finalVersion = "1.22.5";
 in
 (final: prev: {
   go_1_22 = prev.go_1_22.overrideAttrs (oldAttrs: { 
     version = finalVersion;
     src = prev.fetchurl {
       url = "https://go.dev/dl/go${finalVersion}.src.tar.gz";
-      hash = "sha256-/tcgZ45yinyjC6jR3tHKr+J9FgKPqwIyuLqOIgCPt4Q=";
+      hash = "sha256-rJxyPyJJaa7mJLw0/TTJ4T8qIS11xxyAfeZEu0bhEvY=";
     };
   });
 })
