f3ee54c1decdca3bb3c4f7af28862cbb27be4e7f
Joseph Hanson <joe@veri.dev>
2024-07-11 07:28:42 -0500

    Add telchar.

--- nixos/profiles/global/users.nix
+++ nixos/profiles/global/users.nix
@@ -30,6 +30,7 @@ in
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIDJtqzSFK3MN12Lo3Y4DnzJV5NiygIPkR+gun5oEb2q jahanson@legiondary"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBsUe5YF5z8vGcEYtQX7AAiw2rJygGf2l7xxr8nZZa7w jahanson@durincore"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILcLI5qN69BuoLp8p7nTYKoLdsBNmZB31OerZ63Car1g jahanson@telchar"
     ];
   };
 }
