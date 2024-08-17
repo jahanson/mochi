e55e715ebe9d0a636d5b51fbc2f5b53cb3a108a5
Joseph Hanson <joe@veri.dev>
2024-08-16 21:50:57 -0500

    override any alias used for ls

--- nixos/profiles/global/system.nix
+++ nixos/profiles/global/system.nix
@@ -4,7 +4,7 @@
     # Enable printing changes on nix build etc with nvd
     activationScripts.report-changes = ''
       PATH=$PATH:${lib.makeBinPath [ pkgs.nvd pkgs.nix ]}
-      nvd diff $(ls -dv /nix/var/nix/profiles/system-*-link | tail -2)
+      nvd diff $(${pkgs.coreutils}/bin/ls -dv /nix/var/nix/profiles/system-*-link | tail -2)
     '';
 
     # Do not change unless you know what you are doing
