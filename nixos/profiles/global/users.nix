ccb32a88e07e2777d3b91a791e89b4fc7932e55b
Joseph Hanson <joe@veri.dev>
2024-09-12 10:16:24 -0500

    time to officially add /that/ system user

--- nixos/profiles/global/users.nix
+++ nixos/profiles/global/users.nix
@@ -10,27 +10,43 @@ in
     };
   };
 
-  users.users.jahanson = {
-    isNormalUser = true;
-    shell = pkgs.fish;
-    hashedPasswordFile = config.sops.secrets.jahanson-password.path;
-    extraGroups =
-      [
-        "wheel"
-      ]
-      ++ ifTheyExist [
-        "network"
-        "samba-users"
-        "docker"
-        "podman"
-        "audio" # pulseaudio
-        "libvirtd"
-      ];
+  users = {
+    groups = {
+      kah = {
+        gid = 568;
+      };
+    };
+    users = {
+      kah = {
+        isSystemUser = true;
+        group = "kah";
+        uid = 568;
+      };
 
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIDJtqzSFK3MN12Lo3Y4DnzJV5NiygIPkR+gun5oEb2q jahanson@legiondary"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBsUe5YF5z8vGcEYtQX7AAiw2rJygGf2l7xxr8nZZa7w jahanson@durincore"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILcLI5qN69BuoLp8p7nTYKoLdsBNmZB31OerZ63Car1g jahanson@telchar"
-    ];
+      jahanson = {
+        isNormalUser = true;
+        shell = pkgs.fish;
+        hashedPasswordFile = config.sops.secrets.jahanson-password.path;
+        extraGroups =
+          [
+            "wheel"
+            "kah"
+          ]
+          ++ ifTheyExist [
+            "network"
+            "samba-users"
+            "docker"
+            "podman"
+            "audio" # pulseaudio
+            "libvirtd"
+          ];
+
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIDJtqzSFK3MN12Lo3Y4DnzJV5NiygIPkR+gun5oEb2q jahanson@legiondary"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBsUe5YF5z8vGcEYtQX7AAiw2rJygGf2l7xxr8nZZa7w jahanson@durincore"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILcLI5qN69BuoLp8p7nTYKoLdsBNmZB31OerZ63Car1g jahanson@telchar"
+        ];
+      };
+    };
   };
 }
