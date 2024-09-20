e24f915f9c71decb51656aaece4d938b027be3d2
Joseph Hanson <joe@veri.dev>
2024-09-20 09:13:22 -0500

    smartctl workaround addition of scheduled tests

--- nixos/hosts/shadowfax/config/disks.nix
+++ nixos/hosts/shadowfax/config/disks.nix
@@ -4,6 +4,10 @@
   "/dev/disk/by-id/nvme-SOLIDIGM_SSDPFKNU020TZ_PHEH3142017H2P0C"
   "/dev/disk/by-id/nvme-SOLIDIGM_SSDPFKNU020TZ_PHEH314201AD2P0C"
   "/dev/disk/by-id/nvme-SOLIDIGM_SSDPFKNU020TZ_PHEH314201E72P0C"
+  "/dev/nvme0" # These are required to fix a smartctl bug I have yet to upgrade to a version that fixes it.
+  "/dev/nvme1"
+  "/dev/nvme2"
+  "/dev/nvme3"
   "/dev/disk/by-id/scsi-35000cca23bc8a504"
   "/dev/disk/by-id/scsi-35000cca23bd29918"
   "/dev/disk/by-id/scsi-35000cca23bd29970"
