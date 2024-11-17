db998ac3351ed35394699ad731e0f9c83269019a
Joseph Hanson <joe@veri.dev>
2024-11-17 11:03:38 -0600

    correct versioning

--- nixos/modules/nixos/containers/plex/default.nix
+++ nixos/modules/nixos/containers/plex/default.nix
@@ -6,7 +6,7 @@
 with lib;
 let
   app = "plex";
-  # renovate: depName=ghcr.io/onedr0p/plex datasource=docker
+  # renovate: depName=ghcr.io/onedr0p/plex datasource=docker versioning=loose
   version = "1.40.1.8227-c0dd5a73e@sha256:a60bc6352543b4453b117a8f2b89549e458f3ed8960206d2f3501756b6beb519";
   image = "ghcr.io/onedr0p/plex:${version}";
   user = "kah"; # string
