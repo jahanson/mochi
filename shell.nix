a5ce6a01dd23dbad8e5f90f890891936963c0be8
Joseph Hanson <joe@veri.dev>
2024-11-19 18:14:25 -0600

    fedora jank

--- shell.nix
+++ shell.nix
@@ -6,6 +6,11 @@ in
 pkgs.mkShell {
   # Enable experimental features without having to specify the argument
   NIX_CONFIG = "experimental-features = nix-command flakes";
+  shellHook = ''
+    export TMP=$(mktemp -d "/tmp/nix-shell-XXXXXX")
+    export TEMP=$TMP
+    export TMPDIR=$TMP
+  '';
 
   nativeBuildInputs = with pkgs; [
     cachix
