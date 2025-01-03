4aa0b15af887dd1a1a99c80d7a513dcf73626d96
Joseph Hanson <joe@veri.dev>
2025-01-03 11:43:29 -0600

    haproxy for talos k8s api

--- nixos/hosts/telperion/config/haproxy.nix
+++ nixos/hosts/telperion/config/haproxy.nix
@@ -21,25 +21,11 @@
     timeout http-keep-alive 10s
     timeout check 10s
 
-  frontend k8s_homelab_apiserver
+  frontend k8s_theshire_apiserver
     bind *:6443
     mode tcp
     option tcplog
-    default_backend k8s_homelab_controlplane
-
-  frontend k8s_theshire_apiserver
-    bind *:6444
-    mode tcp
-    option tcplog
     default_backend k8s_theshire_controlplane
-
-  backend k8s_homelab_controlplane
-    option httpchk GET /healthz
-    http-check expect status 200
-    mode tcp
-    option ssl-hello-chk
-    balance roundrobin
-    server shadowfax 10.1.1.61:6443 check
 
   backend k8s_theshire_controlplane
     option httpchk GET /healthz
