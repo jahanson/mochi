4b81e0789ad0e3b52094b95756772347f653157b
Joseph Hanson <joe@veri.dev>
2025-01-06 00:00:18 -0600

    correct backup dir

--- nixos/modules/nixos/services/forgejo/default.nix
+++ nixos/modules/nixos/services/forgejo/default.nix
@@ -33,7 +33,7 @@ in
       # enable sql db dumps daily
       dump = {
         enable = true;
-        backupDir = "/mnt/backup/forgejo/backups/";
+        backupDir = "/mnt/storagebox/forgejo/backup/";
       };
       database.type = "postgres";
       # Enable support for Git Large File Storage
