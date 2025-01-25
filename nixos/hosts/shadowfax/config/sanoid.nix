b79062c4ea4641d8e9e7f9359a4270421c4b2d35
Joseph Hanson <joe@veri.dev>
2025-01-24 19:32:45 -0600

    enable snaps for sabnzbd and qbittorrent appdata

--- nixos/hosts/shadowfax/config/sanoid.nix
+++ nixos/hosts/shadowfax/config/sanoid.nix
@@ -10,22 +10,14 @@
         daily = 7;
         monthly = 12;
       };
-      "nvr" = {
-        autoprune = true;
-        autosnap = true;
-        hourly = 24;
-        daily = 7;
-      };
     };
+
     datasets = {
-      "nahar/scrypted" = {
-        useTemplate = [ "nvr" ];
-      };
-      "nahar/containers/volumes/plex" = {
+      "nahar/qbittorrent" = {
         useTemplate = [ "production" ];
         recursive = true;
       };
-      "nahar/containers/volumes/scrypted" = {
+      "nahar/sabnzbd" = {
         useTemplate = [ "production" ];
         recursive = true;
       };
@@ -33,7 +25,15 @@
         useTemplate = [ "production" ];
         recursive = true;
       };
+      "nahar/containers/volumes/plex" = {
+        useTemplate = [ "production" ];
+        recursive = true;
+      };
       "nahar/containers/volumes/scrutiny" = {
+        useTemplate = [ "production" ];
+        recursive = true;
+      };
+      "nahar/containers/volumes/scrypted" = {
         useTemplate = [ "production" ];
         recursive = true;
       };
