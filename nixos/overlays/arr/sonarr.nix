12f1164e0cb5b690f7d7c7db90ff638d6f63655d
Joseph Hanson <joe@veri.dev>
2025-02-04 12:02:57 -0600

    pull straight from servarr

--- nixos/overlays/arr/sonarr.nix
+++ nixos/overlays/arr/sonarr.nix
@@ -12,9 +12,11 @@
   openssl,
   nixosTests,
   zlib,
-}:
-let
-  os = if stdenv.hostPlatform.isDarwin then "osx" else "linux";
+}: let
+  os =
+    if stdenv.hostPlatform.isDarwin
+    then "osx"
+    else "linux";
   arch =
     {
       x86_64-linux = "x64";
@@ -22,37 +24,45 @@ let
       x86_64-darwin = "x64";
       aarch64-darwin = "arm64";
     }
-    ."${stdenv.hostPlatform.system}" or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
+    ."${stdenv.hostPlatform.system}"
+    or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
 
   hash =
     {
-      x64-linux_hash = "sha256-/vsU0M8jyyfYVpE75gSa7R6aSyhxBg8gO3c+G3qCA+E=";
+      x64-linux_hash = "sha256-M2qpyV8fdFleWge1k1EUEeGFg4G7caxkShIriac8bYk=";
       arm64-linux_hash = lib.fakeSha256;
       x64-osx_hash = lib.fakeSha256;
       arm64-osx_hash = lib.fakeSha256;
     }
     ."${arch}-${os}_hash";
 in
-stdenv.mkDerivation rec {
-  pname = "sonarr";
-  version = "4.0.12.2823";
+  stdenv.mkDerivation rec {
+    pname = "sonarr";
+    version = "4.0.12.2900";
+    branch = "develop";
 
-  src = fetchurl {
-    url = "https://github.com/Sonarr/Sonarr/releases/download/v${version}/Sonarr.main.${version}.${os}-${arch}.tar.gz";
-    sha256 = hash;
-  };
+    src = fetchurl {
+      # url = "https://github.com/Sonarr/Sonarr/releases/download/v${version}/Sonarr.main.${version}.${os}-${arch}.tar.gz";
+      url = "https://services.sonarr.tv/v1/update/${branch}/download?version=${version}&os=${os}&runtime=netcore&arch=${arch}";
+      sha256 = hash;
+    };
 
-  nativeBuildInputs = [ makeWrapper ];
+    nativeBuildInputs = [makeWrapper];
 
-  installPhase = ''
-    runHook preInstall
+    unpackPhase = ''
+      mkdir -p $out
+      tar -zxf $src --strip-components=1
+    '';
 
-    mkdir -p $out/{bin,share/${pname}-${version}}
-    cp -r * $out/share/${pname}-${version}/.
+    installPhase = ''
+      runHook preInstall
 
-    makeWrapper "${dotnet-runtime}/bin/dotnet" $out/bin/Sonarr \
-      --add-flags "$out/share/${pname}-${version}/Sonarr.dll" \
-      --prefix LD_LIBRARY_PATH : ${
+      mkdir -p $out/{bin,share/${pname}-${version}}
+      cp -r * $out/share/${pname}-${version}/.
+
+      makeWrapper "${dotnet-runtime}/bin/dotnet" $out/bin/Sonarr \
+        --add-flags "$out/share/${pname}-${version}/Sonarr.dll" \
+        --prefix LD_LIBRARY_PATH : ${
         lib.makeLibraryPath [
           curl
           sqlite
@@ -64,11 +74,11 @@ stdenv.mkDerivation rec {
         ]
       }
 
-    runHook postInstall
-  '';
-  passthru = {
-    tests.smoke-test = nixosTests.radarr;
-  };
+      runHook postInstall
+    '';
+    passthru = {
+      tests.smoke-test = nixosTests.radarr;
+    };
 
-  meta.mainProgram = "Sonarr";
-}
+    meta.mainProgram = "Sonarr";
+  }
