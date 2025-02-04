de9e778a9d518e835b2c9d915b921d90a1a8d7f3
Joseph Hanson <joe@veri.dev>
2025-02-04 11:34:33 -0600

    update radarr

--- nixos/overlays/arr/radarr.nix
+++ nixos/overlays/arr/radarr.nix
@@ -12,9 +12,11 @@
   openssl,
   nixosTests,
   zlib,
-}:
-let
-  os = if stdenv.hostPlatform.isDarwin then "osx" else "linux";
+}: let
+  os =
+    if stdenv.hostPlatform.isDarwin
+    then "osx"
+    else "linux";
   arch =
     {
       x86_64-linux = "x64";
@@ -22,37 +24,38 @@ let
       x86_64-darwin = "x64";
       aarch64-darwin = "arm64";
     }
-    ."${stdenv.hostPlatform.system}" or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
+    ."${stdenv.hostPlatform.system}"
+    or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
 
   hash =
     {
-      x64-linux_hash = "sha256-08jQhaPPS4zEQuJ2ovP/ZsNXG1dJvia4X6RrXywHNao=";
-      arm64-linux_hash = "sha256-70IWCu48jvoTHo8Q/78n/ZDmhFwm9PZOYXUl/17insg=";
-      x64-osx_hash = "sha256-fJAjGx5l4wa27krZFAeKvrWDk9p02HtXhbDv04x0+sA=";
-      arm64-osx_hash = "sha256-GmWDseb8MrpKIL50KAbTFjzu1MoEZXpzUI44Xwaeoeo=";
+      x64-linux_hash = "sha256-D0Np9Jz7E4/1dnWkFdHQIGthklCVc6yav2AAE9pFcu0=";
+      arm64-linux_hash = lib.fakeSha256;
+      x64-osx_hash = lib.fakeSha256;
+      arm64-osx_hash = lib.fakeSha256;
     }
     ."${arch}-${os}_hash";
 in
-stdenv.mkDerivation rec {
-  pname = "radarr";
-  version = "5.17.2.9580";
+  stdenv.mkDerivation rec {
+    pname = "radarr";
+    version = "5.18.4.9674";
 
-  src = fetchurl {
-    url = "https://github.com/Radarr/Radarr/releases/download/v${version}/Radarr.master.${version}.${os}-core-${arch}.tar.gz";
-    sha256 = hash;
-  };
+    src = fetchurl {
+      url = "https://github.com/Radarr/Radarr/releases/download/v${version}/Radarr.master.${version}.${os}-core-${arch}.tar.gz";
+      sha256 = hash;
+    };
 
-  nativeBuildInputs = [ makeWrapper ];
+    nativeBuildInputs = [makeWrapper];
 
-  installPhase = ''
-    runHook preInstall
+    installPhase = ''
+      runHook preInstall
 
-    mkdir -p $out/{bin,share/${pname}-${version}}
-    cp -r * $out/share/${pname}-${version}/.
+      mkdir -p $out/{bin,share/${pname}-${version}}
+      cp -r * $out/share/${pname}-${version}/.
 
-    makeWrapper "${dotnet-runtime}/bin/dotnet" $out/bin/Radarr \
-      --add-flags "$out/share/${pname}-${version}/Radarr.dll" \
-      --prefix LD_LIBRARY_PATH : ${
+      makeWrapper "${dotnet-runtime}/bin/dotnet" $out/bin/Radarr \
+        --add-flags "$out/share/${pname}-${version}/Radarr.dll" \
+        --prefix LD_LIBRARY_PATH : ${
         lib.makeLibraryPath [
           curl
           sqlite
@@ -64,13 +67,13 @@ stdenv.mkDerivation rec {
         ]
       }
 
-    runHook postInstall
-  '';
+      runHook postInstall
+    '';
 
-  passthru = {
-    updateScript = ./update.sh;
-    tests.smoke-test = nixosTests.radarr;
-  };
+    passthru = {
+      updateScript = ./update.sh;
+      tests.smoke-test = nixosTests.radarr;
+    };
 
-  meta.mainProgram = "Radarr";
-}
+    meta.mainProgram = "Radarr";
+  }
